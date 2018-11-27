4d9d937696b891468253639ae20e482fbb7005d9
Theo Noel <theo.noel59@gmail.com>
2018-11-27 17:45:14 +0100

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  new_article = Article.new(title: Faker::Beer.name, content: Faker::Lorem.paragraph(300))
+  new_article.save
+end
